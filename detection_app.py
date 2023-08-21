b0e36d54445de0718e2be3307bd33038c4b20d3d
Frank E <frankeyaife@yahoo.co.uk>
2023-08-21 22:48:23 +0100

    Updated the detection page.

--- detection_app.py
+++ detection_app.py
@@ -1,28 +1,45 @@
 import tkinter as tk
 from tkinter import filedialog
+import os
 
 
-def start_detection():
+def start_detection(file_path):
+    # Add your code for "Start Detection" here
+    print("Starting detection with file:", file_path)
+
+
+def open_detection_page(selected_file):
     # Create a new window for detection
     detection_window = tk.Toplevel(root)
     detection_window.title("Detection Page")
 
-    # Add a label with summary text
-    summary_text = "This detection system is designed to find anomalous attacks on blockchain networks, with a focus on Bitcoin attacks."
-    summary_label = tk.Label(
-        detection_window, text=summary_text, font=("Arial", 12))
-    summary_label.pack(pady=20)
+    # Add a label with the selected file name (not the full path)
+    selected_file_name = os.path.basename(selected_file)
+    selected_file_label = tk.Label(
+        detection_window, text="File selected: " + selected_file_name, font=("Arial", 12))
+    selected_file_label.pack(pady=10)
 
-    # Function to handle file selection
-    def select_file():
-        file_path = filedialog.askopenfilename()
-        # Here, you can use the selected file_path for your detection logic
-        print("Selected file:", file_path)
+    # Add text with a message
+    message_text = "This file will not go through the detection process. Any anomalous attacks will be declared."
+    message_label = tk.Label(
+        detection_window, text=message_text, font=("Arial", 12))
+    message_label.pack(pady=10)
 
-    # Add a button for file selection
-    select_file_button = tk.Button(
-        detection_window, text="Select File to Detect", command=select_file, **button_style)
-    select_file_button.pack(pady=10)
+    # Add a button for confirmation
+    confirm_button = tk.Button(detection_window, text="Confirm",
+                               command=lambda: start_detection(selected_file), **button_style)
+    confirm_button.pack(pady=10)
+
+    # Add a button to cancel and close the detection window
+    cancel_button = tk.Button(
+        detection_window, text="Cancel", command=detection_window.destroy, **button_style)
+    cancel_button.pack(pady=10)
+
+
+def select_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        open_detection_page(file_path)
 
 
 def train_model():
@@ -55,7 +72,7 @@ button_style = {
     "activebackground": "gray"
 }
 
-start_button = tk.Button(root, text="Start Detection",
+start_button = tk.Button(root, text="Detect Attack",
                          command=select_file, **button_style)
 start_button.pack(pady=10)
 
