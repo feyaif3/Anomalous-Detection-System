19ad0b4197bd0239c894a1154cff3e5f57a436f9
Frank E <frankeyaife@yahoo.co.uk>
2023-08-21 22:01:34 +0100

    Added pages to the buttons

--- detection_app.py
+++ detection_app.py
@@ -1,9 +1,28 @@
 import tkinter as tk
+from tkinter import filedialog
 
 
 def start_detection():
-    # Add your code for "Start Detection" here
-    pass
+    # Create a new window for detection
+    detection_window = tk.Toplevel(root)
+    detection_window.title("Detection Page")
+
+    # Add a label with summary text
+    summary_text = "This detection system is designed to find anomalous attacks on blockchain networks, with a focus on Bitcoin attacks."
+    summary_label = tk.Label(
+        detection_window, text=summary_text, font=("Arial", 12))
+    summary_label.pack(pady=20)
+
+    # Function to handle file selection
+    def select_file():
+        file_path = filedialog.askopenfilename()
+        # Here, you can use the selected file_path for your detection logic
+        print("Selected file:", file_path)
+
+    # Add a button for file selection
+    select_file_button = tk.Button(
+        detection_window, text="Select File to Detect", command=select_file, **button_style)
+    select_file_button.pack(pady=10)
 
 
 def train_model():
@@ -21,8 +40,8 @@ root.title("Anomalous Attack Detection System")  # Set the title
 root.geometry("600x500")  # Set the window size
 
 # Customize the title label (make it red)
-title_label = tk.Label(root, text="Anomalous Attack Detection System", font=(
-    "Arial", 20), fg="red")
+title_label = tk.Label(
+    root, text="Anomalous Attack Detection System", font=("Arial", 20), fg="red")
 title_label.pack(pady=20)
 
 # Customize the buttons (making them rounded)
