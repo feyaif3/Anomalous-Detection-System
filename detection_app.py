02174f90c91d7ebc691a581f3067c62bc1b5f16f
Frank E <74784382+feyaif3@users.noreply.github.com>
2023-09-20 21:26:25 +0100

    GUI scrapped and terminal script created

--- detection_app.py
+++ detection_app.py
@@ -1,87 +1,48 @@
+import os
 import tkinter as tk
 from tkinter import filedialog
-import os
 
+def display_menu():
+    print("Anomalous Detection Application")
+    print("1. Detect Attack")
+    print("2. Train Model")
+    print("3. Exit")
 
-def start_detection(file_path):
-    # Add your code for "Start Detection" here
-    print("Starting detection with file:", file_path)
+def detect_attack():
+    print("Detecting attack...")
 
+    root = tk.Tk()
+    root.withdraw()  # Hide the main tkinter window
 
-def open_detection_page(selected_file):
-    # Create a new window for detection
-    detection_window = tk.Toplevel(root)
-    detection_window.title("Detection Page")
+    # Ask the user to select a file using a file dialog
+    file_path = filedialog.askopenfilename(title="Select a file for analysis")
 
-    # Add a label with the selected file name (not the full path)
-    selected_file_name = os.path.basename(selected_file)
-    selected_file_label = tk.Label(
-        detection_window, text="File selected: " + selected_file_name, font=("Arial", 12))
-    selected_file_label.pack(pady=10)
-
-    # Add text with a message
-    message_text = "This file will go through the detection process. Any anomalous attacks will be declared."
-    message_label = tk.Label(
-        detection_window, text=message_text, font=("Arial", 12))
-    message_label.pack(pady=10)
-
-    # Add a button for confirmation
-    confirm_button = tk.Button(detection_window, text="Confirm",
-                               command=lambda: start_detection(selected_file), **button_style)
-    confirm_button.pack(pady=10)
-
-    # Add a button to cancel and close the detection window
-    cancel_button = tk.Button(
-        detection_window, text="Cancel", command=detection_window.destroy, **button_style)
-    cancel_button.pack(pady=10)
-
-
-def select_file():
-    file_path = filedialog.askopenfilename()
     if file_path:
-        open_detection_page(file_path)
+        print(f"Selected file: {file_path}")
+        confirmation = input("Do you want to start detecting? (confirm/cancel): ")
 
+        if confirmation.lower() == "confirm":
+            # Add your code here to start detecting
+            print("Detection process started...")
+        else:
+            print("Detection canceled.")
+    else:
+        print("No file selected. Detection canceled.")
 
 def train_model():
-    # Add your code for "Train Model" here
-    pass
+    print("Training model...")
+    # Add your code for model training here
 
-
-def exit_app():
-    root.destroy()
-
-
-# Create the main window
-root = tk.Tk()
-root.title("Anomalous Attack Detection System")  # Set the title
-root.geometry("600x500")  # Set the window size
-
-# Customize the title label (make it red)
-title_label = tk.Label(
-    root, text="Anomalous Attack Detection System", font=("Arial", 20), fg="red")
-title_label.pack(pady=20)
-
-# Customize the buttons (making them rounded)
-button_style = {
-    "font": ("Helvetica", 12),
-    "relief": tk.RAISED,
-    "borderwidth": 3,
-    "width": 15,
-    "height": 1,
-    "bg": "lightgray",
-    "activebackground": "gray"
-}
-
-start_button = tk.Button(root, text="Detect Attack",
-                         command=select_file, **button_style)
-start_button.pack(pady=10)
-
-train_button = tk.Button(root, text="Train Model",
-                         command=train_model, **button_style)
-train_button.pack(pady=10)
-
-exit_button = tk.Button(root, text="Exit", command=exit_app, **button_style)
-exit_button.pack(pady=10)
-
-# Start the Tkinter main loop
-root.mainloop()
+while True:
+    display_menu()
+    choice = input("Enter your choice (1/2/3): ")
+    
+    if choice == '1':
+        detect_attack()
+    elif choice == '2':
+        train_model()
+    elif choice == '3':
+        print("Exiting the application. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please select a valid option (1/2/3).")
---
+++ hybrid_deep_model.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Load CSV data
+data = pd.read_csv('your_data.csv')
+
+# Preprocess data (excluding 'label' column)
+features = data.drop(columns=['label']).values
+labels = data['label'].values
+
+# Define LSTM Component
+lstm_input = keras.Input(shape=(features.shape[1],), name='lstm_input')
+lstm_layer = layers.LSTM(64)(lstm_input)
+
+# Define CNN Component
+cnn_input = keras.Input(shape=(features.shape[1],), name='cnn_input')
+cnn_layer = layers.Dense(32, activation='relu')(cnn_input)
+
+# Define Autoencoder Component
+autoencoder_input = keras.Input(shape=(features.shape[1],), name='autoencoder_input')
+encoded = layers.Dense(128, activation='relu')(autoencoder_input)
+decoded = layers.Dense(features.shape[1], activation='sigmoid')(encoded)
+
+autoencoder = keras.Model(inputs=autoencoder_input, outputs=decoded)
+autoencoder.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the Autoencoder on normal data
+normal_data = features[labels == 0]
+autoencoder.fit(normal_data, normal_data, epochs=10, batch_size=32)
+
+# Define the anomaly detection threshold (adjust as needed)
+threshold = 0.1
+
+# Detect anomalies using the autoencoder
+reconstruction_errors = np.mean(np.square(features - autoencoder.predict(features)), axis=1)
+anomalies = (reconstruction_errors > threshold).astype(int)
+
+# Combine Components
+combined_input = layers.concatenate([lstm_layer, cnn_layer, autoencoder.output])
+
+# Create the Output Layer for Anomaly Detection
+output_layer = layers.Dense(1, activation='sigmoid')(combined_input)
+
+# Create the Hybrid Model
+model = keras.Model(inputs=[lstm_input, cnn_input, autoencoder_input], outputs=output_layer)
+
+# Compile the Model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the Model
